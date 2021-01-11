30f2b8c9aaa3d794b2085c56dbbd0454c17cd380
Gaurav Kamra <kamra.gaurav515@gmail.com>
2021-01-11 23:29:19 +0530

    First Commit

---
+++ src/main/java/com/gaurav/user/vo/Department.java
@@ -0,0 +1,22 @@
+package com.gaurav.user.vo;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class Department {
+    private Long departmentId;
+    private String departmentName;
+    private String departmentAddress;
+    private String departmaneCode;
+
+}
